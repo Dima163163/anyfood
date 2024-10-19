8db8d2b042a8f0946ac1248497e463bd277d5f40
Dima163163 <khlopovdima@gmail.com>
2024-10-19 13:31:08 +0400

    fix name user review

--- src/components/ItemReview/ItemReview.jsx
+++ src/components/ItemReview/ItemReview.jsx
@@ -1,9 +1,29 @@
+import { useGetUsersQuery } from '../../redux/services/api/api';
 import Button from '../Button/Button';
+import { Loader } from '../Loader/Loader';
 
 export const ItemReview = ({ text, userId, rating }) => {
+  const { data, isLoading, isError } = useGetUsersQuery();
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  if (!data?.length) {
+    return null;
+  }
+  
+  const user = data.find(user => user.id === userId);
+
   return (
     <li>
-      <p>{userId}: {text}. Рейтинг: {rating}</p>
+      <p>
+        {user.name}: {text}. Рейтинг: {rating}
+      </p>
       <Button
         onClick={() => {}}
         text='Редактировать'
