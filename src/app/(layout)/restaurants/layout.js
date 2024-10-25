681f846e0f90860b2fec23a6be2d9e767122a706
Dima163163 <khlopovdima@gmail.com>
2024-10-25 19:08:00 +0400

    fix revalidatePath

--- src/app/(layout)/restaurants/layout.js
+++ src/app/(layout)/restaurants/layout.js
@@ -2,11 +2,9 @@ import { Suspense } from 'react';
 import { RestaurantsPage } from '../../../pages/RestaurantsPage/RestaurantsPage';
 import { getRestaurants } from '../../services/getRestaurants';
 import Loading from './loading';
-import { revalidatePath } from 'next/cache';
 
 export default async function RestaurantsLayout({children}) {
   const restaurants = await getRestaurants();
-  revalidatePath('/restaurants');
 
   return (
     <Suspense fallback={<Loading/>}>
