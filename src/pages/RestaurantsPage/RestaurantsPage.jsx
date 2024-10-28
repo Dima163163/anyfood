37b6d23d5ac0e6f7e066333758716503a479ecdf
Dima163163 <khlopovdima@gmail.com>
2024-10-28 13:21:53 +0400

    fix routing page restaurant

--- src/pages/RestaurantsPage/RestaurantsPage.jsx
+++ src/pages/RestaurantsPage/RestaurantsPage.jsx
@@ -1,12 +1,19 @@
 'use client'
+import { useEffect } from 'react';
 import { Container } from '../../components/Container/Container';
 import { RestaurantsTabsWrapper } from '../../components/RestaurantsTabsWrapper/RestaurantsTabsWrapper';
 import { RestaurantTab } from '../../components/RestaurantTab/RestaurantTab';
 import { ResturantsTabsSection } from '../../components/ResturantsTabsSection/ResturantsTabsSection';
 
 import styles from './RestaurantsPage.module.css';
+import { useRouter } from 'next/navigation';
 
 export const RestaurantsPage = ({ restaurants, children }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`/restaurants/${restaurants[0].id}`)
+  })
 
   return (
     <main className={styles.restaurantsPage}>
